90c5981ff381d355a9351e6fcda7436e423ea983
League <league@iMac-2.attlocal.net>
2018-12-06 16:58:38 -0800

    badger tasty tomato

---
+++ src/Badger.java
@@ -0,0 +1,16 @@
+
+public class Badger {
+	public static void main(String[] args) {
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 8; j++) {
+				System.out.println("Badger");
+			}
+			for (int k = 0; k < 2; k++) {
+				System.out.println("mushroom");
+			}
+		}
+		for (int m = 0; m < 4; m++) {
+			System.out.println("Panda");
+		}
+	}
+}
