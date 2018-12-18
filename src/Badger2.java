ee9cc5d38f60f9de98732b6f7b3c3969b4e9245f
audreyizadi <league@WorthyMini-5.attlocal.net>
2018-12-17 16:57:58 -0800

    badger

---
+++ src/Badger2.java
@@ -0,0 +1,28 @@
+
+public class Badger2 {
+	public static void main(String[] args) {
+		for (int a = 0; a<2; a++) {
+			for (int u = 0; u<8; u++) {
+				System.out.print("Badger, ");
+				/* for (int d = 0; d<2; d++) {
+					 System.out.println("Mushroom"); {
+						 for ( int r = 0; r<4; r++ ) {
+							 System.out.println("Snake");
+						 }
+					 }
+					 
+				 }
+*/			}
+			System.out.println();
+			for (int d = 0; d<2; d++){
+			System.out.print("Mushroom, ");
+			
+			
+			}
+			System.out.println();
+		
+		}
+		
+	}
+
+}
